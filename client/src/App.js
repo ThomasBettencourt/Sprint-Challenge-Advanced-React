ef8ffb6b4ed6629811169f7a62c7df531a882a69
ThomasBettencourt <thomasbetten.cs@gmail.com>
2020-04-17 18:41:18 -0500

    trying to debug / fix axios

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,37 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from 'react';
 
-function App() {
+import Container from '@material-ui/core/Container';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Switch from '@material-ui/core/Switch';
+import Typography from '@material-ui/core/Typography';
+
+import {ThemeProvider} from '@material-ui/core/styles';
+import CSSBaseline from '@material-ui/core/CssBaseline';
+import {useTheme} from '@material-ui/core/styles';
+
+import {lightTheme, darkTheme} from './themes';
+import CardList from './components/CardList';
+import { useDarkMode } from './hooks/useDarkMode';
+
+export default function App() {
+  
+  const [isDarkTheme, setIsDarkTheme] = useDarkMode('isDarkMode', false);
+  const theme = useTheme();
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
-
-export default App;
+    <ThemeProvider theme={isDarkTheme ? darkTheme : lightTheme}>
+    <CSSBaseline />
+ 
+    <Container maxWidth='md' style={{margin: '0 auto'}}>
+    <AppBar position='relative' color='default' style={{marginBottom: theme.spacing(6), transition: 'background-color 4s ease'}}>
+       <Toolbar  style={{display: 'flex', alignContent: 'center', alignItems: 'center', justifyContent: 'space-between'}}>
+        <Typography variant='h5' style={{transition: 'color 4s ease'}}>Player searches</Typography>
+         <Typography variant='body1'>Use dark mode?<Switch color='primary' checked={isDarkTheme} onChange={() => {setIsDarkTheme(!isDarkTheme)}}/></Typography>
+       </Toolbar>
+     </AppBar>
+    <CardList/>
+   </Container>
+ </ThemeProvider>
+)
+  }
---
+++ client/src/components/PlayerCard.js
@@ -0,0 +1,42 @@
+import React from 'react';
+import Card from '@material-ui/core/Card';
+import CardActions from '@material-ui/core/CardActions';
+import CardContent from '@material-ui/core/CardContent';
+import Typography from '@material-ui/core/Typography';
+
+import {useTheme, makeStyles} from '@material-ui/core/styles'
+import useMediaQuery from '@material-ui/core/useMediaQuery';
+
+
+const useStyles = makeStyles({
+    large: {
+      width: '250px',
+      height: '150px',
+      transition: 'background-color 4s ease'
+    },
+    mobile: {
+        width: '400px',
+        height: '150px',
+        transition: 'background-color 4s ease'
+    },
+    typography: {
+        transition: 'color 4s ease'
+    }
+  })
+
+export default function PlayerCard(props) {
+        const theme = useTheme()
+        const isMobile = useMediaQuery(theme.breakpoints.down('xs'))
+        const classes = useStyles();
+
+     return (
+        <Card  className={isMobile ? classes.mobile : classes.large}>
+            <CardContent>
+                <Typography variant="h5" className={classes.typography}>{props.name}</Typography>
+                <Typography variant="h6" className={classes.typography}>{props.country}</Typography>
+                <Typography variant="body1" className={classes.typography}>{props.searches} searches.</Typography>
+            </CardContent>
+        </Card>
+        )
+    
+}
